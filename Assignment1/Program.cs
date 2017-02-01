80a9c0a8228c1b8a6aa189b5c52dfba19963f875
Markus Johan Haugsdal <Mjhaugsdal@hotmail.com>
2017-02-01 04:11:54 -0600

    Switching to enumerated symbols

--- Assignment1/Program.cs
+++ Assignment1/Program.cs
@@ -1,12 +1,21 @@
 ﻿using System;
-using System.Threading;
 using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+//using System.Threading.Tasks;
 using System.Globalization;
 
+/// <summary>
+/// Name: Markus Johan Haugsdal
+/// Class: CSC 446 Compiler Construction
+/// Assignment: 1
+/// Due Date: 01.02.2017
+/// Instructor: Hamer
+/// 
+/// Description: Main Driver for compiler assignment 1
+/// 
+/// </summary>
 
 
 namespace Assignment1
@@ -22,32 +31,34 @@ namespace Assignment1
             string fileName;
             //string token;
 
-            fileName = "test2.adb";
+            //fileName = "test2.adb";
 
-            StreamReader sr = new StreamReader(fileName);
 
+
+            Console.WriteLine(args.Length);
             //if not two arguments
-            if (args.Length != 2)
+            if (args.Length != 1)
             {
-                Console.WriteLine("Usage: program name, filename");
+                Console.WriteLine("Usage: \"program name\".exe, \"filename\"");
             }
 
-           // else
-           // {
-
-          
-                 //classes
-                 lexicalScanner lx = new lexicalScanner(fileName, sr);
+            else
+            {
+                fileName = args[0];
+                StreamReader sr = new StreamReader(fileName);
+                //classes
+                lexicalScanner lx = new lexicalScanner(fileName, sr);
 
                  lexicalScanner.Token token = new lexicalScanner.Token();
 
                  lx.createDictionary();
-                 string output = String.Format("{0,-15}  {1,-15} {2,-15}", "Token", "Lexeme", "Attribute");
+                 string output = String.Format("{0,-15}  {1,-15} ", "Token", "Lexeme");
                  //Console.WriteLine("Token\t\tlexeme\t\tAttribute");
                  Console.WriteLine(output);
 
 
-            int i = 0;
+      
+            int j = 0;
 
                 while (token.token != "eoft")
                 {
@@ -55,13 +66,19 @@ namespace Assignment1
 
                     token = lx.getNextToken();
 
-                lx.printToken(token);
-                i++;
-
-
+                    if(j > 20)
+                    {
+                        Console.WriteLine("Press any key to continue...");
+                        Console.ReadKey();
+                        j = 0;    
+                    }
+                    lx.printToken(token);
+                   
+                    j++;
+                }
+                Console.WriteLine("Tokens processed: " + lexicalScanner.i);
+            
             }
-            Console.WriteLine("Tokens: " + i);
-            //}
         }   
     }
 }
--- Assignment1/lexicalScanner.cs
+++ Assignment1/lexicalScanner.cs
@@ -6,6 +6,16 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Globalization;
 
+/// <summary>
+/// Name: Markus Johan Haugsdal
+/// Class: CSC 446 Compiler Construction
+/// Assignment: 1
+/// Due Date: 01.02.2017
+/// Instructor: Hamer
+/// 
+/// Description: Lexical scanner for determining tokens
+/// 
+/// </summary>
 
 namespace Assignment1
 {
@@ -14,6 +24,7 @@ namespace Assignment1
         
         //variables
         char ch;
+        public static int i = 0;
         //public static string token;
         string lexeme;
         private string fileName;
@@ -21,9 +32,15 @@ namespace Assignment1
         public static bool hasDot = false;
 
 
+        public enum SYMBOL { none, begint, modult, constt, proct, ist, ift, thent, elset, elseift, whilet, loopt,
+                             floatt, integert, chart, gett, putt, endt, ort, remt, modt, andt, eoft, unkownt,
+                             relopt, addopt, assignopt, multopt, lparent, rparent,commat,colont,semicolont,
+                             periodt,idt,literalt,numt };
+
+        //Token object for token building
         public class Token
         {
-            public string token;
+            public SYMBOL token = SYMBOL.none;
             public string lexeme;
             public int value;
             public float valueR;
@@ -33,8 +50,8 @@ namespace Assignment1
         
 
 
-
-        Dictionary<string, string> reswords = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+        //Dictionary for checking reserved word tokens
+        Dictionary<string, SYMBOL> reswords = new Dictionary<string, SYMBOL>(StringComparer.OrdinalIgnoreCase);
 
         public lexicalScanner(string fileName)
         {
@@ -46,18 +63,23 @@ namespace Assignment1
             this.sr = sr;
         }
 
-
+        //Gets next char and iterates position in file by 1
         public char getNextChar()
         {
 
             ch =(char)sr.Read();
             return ch;
         }
+        //Gets next char but does NOT iterate position
         public char peekNextChar()
         {
             return (char)sr.Peek();
         }
 
+        /// <summary>
+        /// Checks first char and starts building the token
+        /// </summary>
+        /// <returns> Object of type "token" </returns>
         public Token getNextToken()
         {
 
@@ -65,28 +87,34 @@ namespace Assignment1
 
             while (!sr.EndOfStream)
             {
-
+                //Only peek! 
                 ch = peekNextChar();
-                if (!Char.IsWhiteSpace(ch) && String.IsNullOrWhiteSpace(token.token))
+                if (!Char.IsWhiteSpace(ch) )
                 {
+                    //If peek was successful, get next char
                     ch = getNextChar();
-                    processToken(token);
+                    processToken(token); //Process the token
 
                 }
+                //If newline or whitespace
                 if (ch == 10 || Char.IsWhiteSpace(ch)) 
                 {
                     ch = getNextChar();
                 }
-                else
+                else 
                 {
                     return token;
                 }
             }//end while eof
 
-            token.token = "eoft";
+            token.token = SYMBOL.eoft; //Final token to signify that eof was reached. (Maybe not necessary)
             return token;
         }
-
+        /// <summary>
+        /// ProcessToken. 
+        /// Creates the lexeme. Checks first position of lexeme to determine what to do.
+        /// </summary>
+        /// <param name="token"></param>
         public void processToken(Token token)
         {
             lexeme = ch.ToString();
@@ -139,7 +167,7 @@ namespace Assignment1
 
             else
             {
-                token.token = "unkownt";
+                token.token = SYMBOL.unkownt;
                 token.lexeme = lexeme;
             }
             // return lexeme;
@@ -148,7 +176,10 @@ namespace Assignment1
         }
 
 
-
+        /// <summary>
+        /// Processes double tokens. 
+        /// </summary>
+        /// <param name="token"></param>
         private void processDoubleToken(Token token)
         {
 
@@ -158,15 +189,18 @@ namespace Assignment1
 
             if (lexeme[0] == 47 || lexeme[0] == 60 || lexeme[0] == 62)
             {
-                token.token = "relopt";
+                token.token = SYMBOL.relopt;
             }
             else if(lexeme[0] == 58)
             {
-                token.token = "assignopt";
+                token.token = SYMBOL.assignopt;
             }
 
         }
-
+        /// <summary>
+        /// Single tokens.
+        /// </summary>
+        /// <param name="token"></param>
         private void processSingleToken(Token token)
         {
             
@@ -175,42 +209,42 @@ namespace Assignment1
             token.lexeme = lexeme;
             if (lexeme[0] == 61 || lexeme[0] == 60 || lexeme[0] == 62)
             {
-                token.token = "relopt";
+                token.token = SYMBOL.relopt;
                 
             }
             else if (lexeme[0] == 43 || lexeme[0] == 45)
             {
-                token.token = "addopt";
+                token.token = SYMBOL.addopt;
                 
             }
             else if(lexeme[0] == 42 || lexeme[0] == 47)
             {
-                token.token = "multopt";
+                token.token = SYMBOL.multopt;
                 
             }
             else if(lexeme[0] == 40)
             {
-                token.token = "lparent";
+                token.token = SYMBOL.lparent;
             }
             else if (lexeme[0] == 41)
             {
-                token.token = "rparent";
+                token.token = SYMBOL.rparent;
             }
             else if (lexeme[0] == 44)
             {
-                token.token = "commat";
+                token.token = SYMBOL.commat;
             }
             else if (lexeme[0] == 58)
             {
-                token.token = "colont";
+                token.token = SYMBOL.colont;
             }
             else if (lexeme[0] == 59)
             {
-                token.token = "semicolont";
+                token.token = SYMBOL.semicolont;
             }
             else if (lexeme[0] == 46)
             {
-                token.token = "periodt";
+                token.token = SYMBOL.periodt;
             }
             else if (lexeme[0] == 34)
             {
@@ -219,7 +253,10 @@ namespace Assignment1
 
 
         }
-
+        /// <summary>
+        /// Processes word tokens. Iterates until it finds illegal character ()
+        /// </summary>
+        /// <param name="token"></param>
         public void processWordToken( Token token)
         {
             //Console.WriteLine();
@@ -258,18 +295,21 @@ namespace Assignment1
             else
             {
 
-                token.token = "idt";
+                token.token = SYMBOL.idt;
             }
             //If lexeme is a reserved word
           
             if(token.lexeme.Length > 17)
             {
-                token.token = "unkownt";
+                token.token = SYMBOL.unkownt;
             }
 
 
         }
-
+        /// <summary>
+        /// String literals, if singleToken detects the opening "
+        /// </summary>
+        /// <param name="token"></param>
         private void processStringLiteral(Token token)
         {
             string literal = "\"";
@@ -300,14 +340,14 @@ namespace Assignment1
                 literal = literal + ch;
                 ch = getNextChar();
                 literal = literal + ch;
-                token.token = "literalt";
+                token.token = SYMBOL.literalt;
                 token.literal = literal;
                 token.lexeme = literal;
             }
             else
             {
                 
-                token.token = "unkownt";
+                token.token = SYMBOL.unkownt;
               
                 token.literal = literal;
                 token.lexeme = literal;
@@ -315,13 +355,16 @@ namespace Assignment1
 
         }
 
-
+        /// <summary>
+        /// Num tokens. Iterates and looks for the . which signifies float/real
+        /// </summary>
+        /// <param name="token"></param>
         public void processNumToken(Token token)
         {
            // getNextChar();
             hasDot = false;
             string nums = lexeme[0].ToString();
-            token.token = "numt";
+            token.token = SYMBOL.numt;
             char p;
             if (char.IsWhiteSpace(ch)) // only one char
             {
@@ -366,7 +409,7 @@ namespace Assignment1
                 {
                     if (ch == 46) // if last char of number is .
                     {
-                        token.token = "unkownt";
+                        token.token = SYMBOL.unkownt;
                         token.lexeme = nums;
                     }
                     else
@@ -389,58 +432,64 @@ namespace Assignment1
 
         }
 
+        /// <summary>
+        /// Dictionary for reserved words
+        /// </summary>
         public void createDictionary()
         {
-            reswords.Add("begin", "begint");
-            reswords.Add("module", "modult");
-            reswords.Add("constant", "constt");
-            reswords.Add("procedure", "proct");
-            reswords.Add("is", "ist");
-            reswords.Add("if", "ift");
-            reswords.Add("then", "thent");
-            reswords.Add("else", "elset");
-            reswords.Add("elseif", "elseift");
-            reswords.Add("while", "whilet");
-            reswords.Add("loop", "loopt");
-            reswords.Add("float", "floatt");
-            reswords.Add("integer", "integert");
-            reswords.Add("char", "chart");
-            reswords.Add("get", "gett");
-            reswords.Add("put", "putt");
-            reswords.Add("end", "endt");
-            reswords.Add("or", "ort");
-            reswords.Add("rem", "remt");
-            reswords.Add("mod", "modt");
-            reswords.Add("and", "andt");
+            reswords.Add("begin", SYMBOL.begint);
+            reswords.Add("module", SYMBOL.modult);
+            reswords.Add("constant", SYMBOL.constt);
+            reswords.Add("procedure", SYMBOL.proct);
+            reswords.Add("is", SYMBOL.ist);
+            reswords.Add("if", SYMBOL.ift);
+            reswords.Add("then", SYMBOL.thent);
+            reswords.Add("else", SYMBOL.elset);
+            reswords.Add("elseif", SYMBOL.elseift);
+            reswords.Add("while", SYMBOL.whilet);
+            reswords.Add("loop", SYMBOL.loopt);
+            reswords.Add("float", SYMBOL.floatt);
+            reswords.Add("integer", SYMBOL.integert);
+            reswords.Add("char", SYMBOL.chart);
+            reswords.Add("get", SYMBOL.gett);
+            reswords.Add("put", SYMBOL.putt);
+            reswords.Add("end", SYMBOL.endt);
+            reswords.Add("or", SYMBOL.ort);
+            reswords.Add("rem", SYMBOL.remt);
+            reswords.Add("mod", SYMBOL.modt);
+            reswords.Add("and", SYMBOL.andt);
 
 
         }
 
+        /// <summary>
+        /// PrintToken method to show all tokens and its attribute
+        /// </summary>
+        /// <param name="token"></param>
         public void printToken(Token token)
         {
             string output = "";
-           /* Tuple<string, string, int, float, string> tuple=
-                new Tuple<string, string, int, float, string>(token.token, token.lexeme,token.value, token.valueR, token.literal);
-                */
-            if (!String.IsNullOrEmpty(token.token))
+            
+            if (Enum.IsDefined(SYMBOL, )
             {
 
+                i++;
                 if (token.token == "numt")
                 {
                     if (lexicalScanner.hasDot == false)
                     {
-                        output = string.Format("{0,-15}  {1,-15} {2,-15}", token.token, token.lexeme, token.value);
+                        output = string.Format("{0,-15}  {1,-15}", token.token, token.value);
                         // Console.WriteLine(token.token + "\t\t" + token.lexeme + "\t\t" + token.value);
                     }
 
                     else
                         //Console.WriteLine(token.token + "\t\t" + token.lexeme + "\t\t" + token.valueR);
-                        output = string.Format("{0,-15}  {1,-15} {2,-15}", token.token, token.lexeme,token.valueR);
+                        output = string.Format("{0,-15}  {1,-15} ", token.token, token.valueR);
                 }
                 else if (token.token == "literalt")
                 {
                     //Console.WriteLine(token.token + "\t\t" + token.lexeme + "\t\t" + token.literal);
-                    output = string.Format("{0,-15}  {1,-15} {2,-15}", token.token, token.lexeme, token.literal);
+                    output = string.Format("{0,-15}  {1,-15} ", token.token, token.lexeme);
 
                 }
                 else
